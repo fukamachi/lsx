f1967d4e8ba93b77bb31199d5c5d667b74a9255c
Eitaro Fukamachi <e.arrows@gmail.com>
2018-09-16 22:09:40 +0900

    Add tests.

--- lsx.asd
+++ lsx.asd
@@ -5,3 +5,9 @@
   :license "BSD 2-Clause"
   :description "Embeddable HTML templating engine with JSX-like syntax"
   :depends-on ("lsx/main"))
+
+(defsystem "lsx/tests"
+  :class :package-inferred-system
+  :depends-on ("rove"
+               "lsx/tests/main")
+  :perform (test-op (o c) (symbol-call :rove '#:run c)))
---
+++ tests/main.lisp
@@ -0,0 +1,59 @@
+(defpackage #:lsx/tests/main
+  (:use #:cl
+        #:rove
+        #:lsx/component
+        #:lsx/html
+        #:lsx/file)
+  (:import-from #:lsx/html
+                #:print-escaped-text)
+  (:import-from #:local-time
+                #:now)
+  (:import-from #:cl-ppcre))
+(in-package #:lsx/tests/main)
+
+(defun esc (v)
+  (with-output-to-string (s)
+    (print-escaped-text v s)))
+
+(deftest escaped-text-tests
+  (ok (equal (esc "Hello") "Hello")
+      "Normal string")
+  (ok (equal (esc "Tiffany & Co.") "Tiffany &amp; Co.")
+      "Escape &")
+  (ok (equal (esc "<danger>") "&lt;danger&gt;")
+      "Escape <, >")
+  (ok (equal (esc "\"LEVI'S\"") "&quot;LEVI&#39;S&quot;")
+      "Escape \", \'"))
+
+(deftest element-tests
+  (testing "Normal element"
+    (let ((br (read-lsx-string "<br/>")))
+      (ok (typep br 'element))
+      (ok (equal (element-name br) "br"))
+      (ok (outputs (render-object br t) "<br>"))))
+  (testing "With attributes & children"
+    (let ((a (read-lsx-string "<a href=\"/hello\">Say Hello</a>")))
+      (ok (typep a 'element))
+      (ok (equal (element-name a) "a"))
+      (ok (outputs (render-object a t) "<a href=\"/hello\">Say Hello</a>"))))
+  (testing "Embed Lisp code"
+    (let ((a (read-lsx-string "<a href=\"/hello\">Say Hello at {(local-time:now)}</a>")))
+      (ok (typep a 'element))
+      (ok (equal (element-name a) "a"))
+      (ok (ppcre:scan
+           "^<a href=\"/hello\">Say Hello at \\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:\\d{2}(\\.\\d{6})?.*</a>$"
+           (with-output-to-string (s)
+             (render-object a s)))))))
+
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (when (find-class 'welcome nil)
+    (setf (find-class 'welcome) nil)))
+
+(deftest custom-component-tests
+  (defcomponent welcome ()
+    (name)
+    (:render (h "h1" () (list (lambda () name)))))
+
+  (let ((welcome (read-lsx-string "<welcome name=\"fukamachi\" />")))
+    (ok (typep welcome 'welcome))
+    (ok (outputs (render-object welcome t) "<h1>fukamachi</h1>"))))
