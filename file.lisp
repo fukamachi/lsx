ef6678a5aa2daef604f9f1a4a85ba9d03ff428f1
Eitaro Fukamachi <e.arrows@gmail.com>
2018-09-16 22:09:08 +0900

    Add with-lsx-syntax & read-lsx-string.

--- file.lisp
+++ file.lisp
@@ -2,15 +2,29 @@
   (:use #:cl)
   (:import-from #:lsx/reader
                 #:enable-lsx-syntax)
-  (:export #:read-lsx-file))
+  (:export #:with-lsx-syntax
+           #:read-lsx-string
+           #:read-lsx-file))
 (in-package #:lsx/file)
 
+(defmacro with-lsx-syntax (&body body)
+  `(let ((*package* *package*)
+         (*readtable* (copy-readtable)))
+     (enable-lsx-syntax)
+     ,@body))
+
+(defun read-lsx-string (string)
+  (check-type string string)
+  (with-lsx-syntax
+    (eval (read-from-string string))))
+
 (defun read-lsx-file (file)
-  (let ((*package* *package*)
-        (*readtable* (copy-readtable))
-        (*load-pathname* file)
+  (check-type file pathname)
+  (let ((*load-pathname* file)
         (*load-truename* file))
-    (enable-lsx-syntax)
-    (let (result)
-      (dolist (form (uiop:read-file-forms file) result)
-        (setf result (eval form))))))
+    (with-lsx-syntax
+      (let ((*package* *package*)
+            (*readtable* (copy-readtable))
+            result)
+        (dolist (form (uiop:read-file-forms file) result)
+          (setf result (eval form)))))))
